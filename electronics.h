b32aa745b954e241627734884e8300c615a580ac
AZWaterWarriors <153463281+AZWaterWarriors@users.noreply.github.com>
2023-12-28 11:00:08 -0700

    Update electronics.h

--- electronics.h
+++ electronics.h
@@ -9,8 +9,12 @@
 #ifndef _ELECTRONICS_H
 #define _ELECTRONICS_H
 
+int setupio(void);
 void fans(int onoff);
 void peltier(int onoff);
 void fans2(int onoff);
+
+int gpiofd;
+unsigned int *gpio;
 
 #endif
