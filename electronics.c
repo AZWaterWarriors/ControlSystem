2d068fc8a79475afd523576e984b4943abe4c030
AZWaterWarriors <153463281+AZWaterWarriors@users.noreply.github.com>
2023-12-28 10:59:06 -0700

    Added setupio routine to mmap gpio

--- electronics.c
+++ electronics.c
@@ -5,12 +5,30 @@
  * Electronics control routines
  *
  */
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <unistd.h>
 
 #include "electronics.h"
 
+int setupio(void);
 void fans(int onoff);
 void peltier(int onoff);
 void fans2(int onoff);
+
+int gpiofd;
+unsigned int *gpio;
+
+int setupio(){
+
+  gpiofd = open("/dev/gpiomem", O_RDWR);
+	if (gpiofd < 0) { return -1; };
+
+	gpio = (unsigned int *)mmap(0, 4096, PROT_READ+PROT_WRITE, MAP_SHARED, gpiofd, 0);
+	
+	return 0;
+
+};
 
 void fans(int onoff){
 
