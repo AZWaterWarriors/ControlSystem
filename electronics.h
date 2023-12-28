d8329950c6ffbbde738778b755fc4c4ebc7e8442
AZWaterWarriors <153463281+AZWaterWarriors@users.noreply.github.com>
2023-12-28 10:45:34 -0700

    Create electronics.h

---
+++ electronics.h
@@ -0,0 +1,16 @@
+/*
+ *
+ * Water Warriors Moisture Harvester Control System
+ *
+ * Electronics control routines
+ *
+ */
+
+#ifndef _ELECTRONICS_H
+#define _ELECTRONICS_H
+
+void fans(int onoff);
+void peltier(int onoff);
+void fans2(int onoff);
+
+#endif
